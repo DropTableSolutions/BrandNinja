cf4670df0abf395cb7652e417e8090b9b0642dc2
Jason M. Simmons II <jmjjj595@gmail.com>
2018-04-26 00:09:51 -0400

    Manual Merge with Master
    Manually merged all files with Master branch

--- js/crawler.js
+++ js/crawler.js
@@ -8,18 +8,18 @@ const fs = require('fs');
 const sqlite3 = require('sqlite3');
 //module to allow direct paths to fileSize
 const path = require('path');
-//module to allow electron functionality
 const electron = require('electron');
-
 const BrowserWindow = electron.remote.BrowserWindow;
 
 
-//Variables for searches
 var maxDepth;
 var startingSite;
 var keywords;
-
-//Variables for html objects
+var savedName;
+var savedStartingPages;
+var savedKeywords;
+var savedDepth;
+var running;
 const crawlNameInput = document.querySelector('.crawlNameInput');
 const startUrlInput = document.querySelector('.startUrlInput');
 const crawlDepthInput = document.querySelector('.crawlDepthInput');
@@ -29,19 +29,22 @@ const status = document.querySelector('#status');
 const runButton = document.querySelector('#run');
 const clearButton = document.querySelector('#clear');
 const toggleSwitch = document.querySelector('.theme');
+const reportBtn = document.querySelector('#reportBtn');
 const dbPath = path.resolve('C:\\Users\\Ansari\\Documents\\GitHub\\BrandNinja\\Searches.db');
 const saveButton = document.querySelector('#history');
 const loadButton = document.querySelector('#file');
-
 /*
-Recursive function for crawling
+recursive function for crawling
  */
+
 let visited = new Set();
+
 function crawl(startingSite, depth) {
-    if (depth < maxDepth) {
+    if (depth < maxDepth && running) {
+        console.log(running);
         getLinks(startingSite, function (sites) { //pulls all the links from a specific page and returns them as an array of strings
             for (var i = 0; i < sites.length; i++) { //for each string we got from the page
-                findTarget(sites[i], depth); //find any of the keywords we want on the page, print out if so
+                findTarget(sites[i]); //find any of the keywords we want on the page, print out if so
                 crawl(sites[i], depth + 1); //crawl all the pages on that page, and increase the depth
             }
         });
@@ -49,20 +52,19 @@ function crawl(startingSite, depth) {
 }
 
 /*
-Runs through a list of sites and tries to find any of the keywords
+runs through a list of sites and tries to find any of the keywords
  */
-function findTarget(site, depth) {
+function findTarget(site) {
     findInPage(keywords, site, function (containsATarget, target, url) {
-        if (containsATarget) {
+        if (containsATarget && target != undefined) {
             //TODO: PRINT TO FILE
-            //writeToFile("Matched a keyword: " + target + " at: " + url + " with a depth of: " + (depth + 1));
-            //console.log("Matched a keyword: " + target + " at: " + url + " with a depth of: " + (depth + 1));
             addToWindow(url, target);
         }
     });
 }
+
 /*
-Returns a list of urls found on a parent url page
+returns a list of urls found on a parent url page
  */
 function getLinks(parentURL, callback) {
     let url = parentURL;
@@ -89,7 +91,7 @@ function getLinks(parentURL, callback) {
 
 
 /*
-Returns true if a page contains one of the target keywords, as well as the URL it was found at, and the target itself;
+returns true if a page contains one of the target keywords, as well as the URL it was found at, and the target itself;
  */
 function findInPage(target, url, callback) {
     if (url != undefined) {
@@ -99,16 +101,24 @@ function findInPage(target, url, callback) {
                 var bodyText = $('html > body').text().toLowerCase();
                 let htmlBodySet = new Set(bodyText.split(' '));
                 var htmlBody = Array.from(htmlBodySet);
-                var targets = target.split(',');
-                for (var i = 0; i < targets.length; i++) {
-                    targets[i].trim();
-                    console.log("SEARCHING: " + targets[i]);
-                    if(htmlBody.includes(target[i]))
-                    {
-                        console.log("MATCH: " + targets[i]  + " at: " + url);
-                        return callback(true, targets[i], url);
+                var targets = target.toLowerCase().split(',');
+                var found = false;
+                var results = [];
+                for (var i = 0; i < htmlBody.length; i++) {
+                    var temp = htmlBody[i].replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+                    var text = temp.replace(/\s{2,}/g, " ");
+                    for (var j = 0; j < targets.length; j++) {
+                        targets[j].trim();
+                        if (text.localeCompare(targets[j]) === 0) {
+                            found = true;
+                            results.push(targets[j]);
+                        }
                     }
                 }
+                let resultSet = new Set(results);
+                let arr = Array.from(resultSet);
+                arr.sort();
+                return callback(found, arr.join('-'), url);
             }
         });
     }
@@ -119,41 +129,39 @@ function findInPage(target, url, callback) {
 
 
 runButton.addEventListener('click', function () {
-	if(validateInputs()) {
-		visited.clear();
-		maxDepth = parseInt(crawlDepthInput.value);
-		crawlName = crawlNameInput.value; //TODO: do something with the name
-		startingSite = startUrlInput.value;
-		keywords = keywordInput.value;
-		run();
-	}
+    visited.clear();
+    maxDepth = parseInt(crawlDepthInput.value);
+    crawlName = crawlNameInput.value; //TODO: do something with the name
+    startingSite = startUrlInput.value;
+    keywords = keywordInput.value;
+    reportBtn.style.opacity = 0;
+    run();
 });
 
 function run() {
+    running = true;
     crawl(startingSite, 0);
     resultsWindow();
 }
+
 let win = null;
-function resultsWindow()
-{
-	win = new BrowserWindow({width: 800, height: 600})
-	win.on('closed', () => {
-		win = null
-	});
-	win.loadURL(`file://${__dirname}/../results.html`);
+
+function resultsWindow() {
+    win = new BrowserWindow({width: 800, height: 600});
+    win.on('closed', () => {
+        win = null
+        running = false;
+    });
+    win.loadURL(`file://${__dirname}/../results.html`);
+}
+
+function addToWindow(url, matchedWord) {
+    win.webContents.send('url', url + '');
+    win.webContents.send('matchedWord', matchedWord + '');
 }
 
 /*
-Adds a result to the results window
-*/
-function addToWindow(url, matchedWord)
-{
-        win.webContents.send('url', url + '');
-        win.webContents.send('matchedWord', matchedWord + '');
-}
-
-/*
-Clears the input fields
+clears the input fields
  */
 clearButton.addEventListener('click', function () {
     startingSite = '';
@@ -164,9 +172,7 @@ clearButton.addEventListener('click', function () {
     crawlDepthInput.value = '';
 });
 
-/*
-Toggles between themes
-*/
+
 toggleSwitch.addEventListener('click', function () {
     if (toggleSwitch.checked == false) {
         changeTheme('#4b4b4b', '#ecf0f1');
@@ -177,7 +183,8 @@ toggleSwitch.addEventListener('click', function () {
 });
 
 /*
-Changes the theme
+changes the theme
+//TODO: remove this, it's not needed
  */
 function changeTheme(background, foreground) {
     console.log('Changing theme');
@@ -192,103 +199,49 @@ function changeTheme(background, foreground) {
 }
 
 /*
-Saves searches to the database
+Saves to database?
+//TODO: documentation, possibly split up into multiple functions
  */
 saveButton.addEventListener('click', function () {
-	//Checks for valid inputs
-	if(validateInputs()) {
-		//Gets values from the input fields
-		var savedName = crawlNameInput.value;
-		var savedStartingPages = startUrlInput.value;
-		var savedKeywords = keywordInput.value;
-		var savedDepth = crawlDepthInput.value;
+    savedName = crawlNameInput.value;
+    savedStartingPages = startUrlInput.value;
+    savedKeywords = keywordInput.value;
+    savedDepth = crawlDepthInput.value;
 
-		//Logs the inputs in the console
-		console.log(savedName + savedStartingPages + savedKeywords + savedDepth);
-		
-		//Checks if database is present
-		var exists = fs.existsSync(dbPath);
-		
-		//Logs if the database is present or not
-		console.log("exists: " + exists);
+    console.log(savedName + savedStartingPages + savedKeywords + savedDepth);
+    var exists = fs.existsSync(dbPath);
+    console.log("exists: " + exists);
 
-		//Creates the connection to the database
-		let db = new sqlite3.Database('dbPath', (err) => {
-			if (err) {
-				console.error(err.message);
-			}
-			console.log('Connected to the Searches database.');
-		});
+    let db = new sqlite3.Database('dbPath', (err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log('Connected to the Searches database.');
+    });
 
-		//Adds the saved inputs to the database
-		db.serialize(function () {
+    db.serialize(function () {
 
-			//Creates the table if it is not present
-			db.run("CREATE TABLE if not exists saved_searches(name TEXT, links LONGTEXT, keywords LONGTEXT, depth INT)");
+        db.run("CREATE TABLE if not exists saved_searches(name TEXT, links LONGTEXT, keywords LONGTEXT, depth INT)");
 
-			//Prepares the statement to prevent SQL injection and inserts values into the table
-			var stmt = db.prepare("INSERT INTO saved_searches VALUES (?,?,?,?)");
-			stmt.run(savedName, savedStartingPages, savedKeywords, savedDepth);
-			stmt.finalize();
-		});
+        var stmt = db.prepare("INSERT INTO saved_searches VALUES (?,?,?,?)");
+        stmt.run(savedName, savedStartingPages, savedKeywords, savedDepth);
+        stmt.finalize();
 
-		db.close();
+        db.each("SELECT * FROM saved_searches", function (err, row) {
+            console.log(row.name + ", " + row.links + ", " + row.keywords + ", " + row.depth);
+        });
+    });
 
-		notification.innerHTML = 'Search saved.';
-	}
+    db.close();
+
+    notification.innerHTML = 'Search saved.';
 });
 
 /*
-Opens a new menu to load searches
+TODO: flesh this out
  */
 loadButton.addEventListener('click', function () {
     window.location.href = path.resolve(__dirname, 'searchSelection.html')
 });
 
-/*
-Validates the inputs to limit errors
-*/
-function validateInputs() {
-	var inputsValid = true;
-	
-	//Regex for each input field
-	//Allows any letters or numbers for search name
-	var nameRegEx = new RegExp('[0-9a-zA-Z ]');
-	
-	//Allows any website in the format 'http(s)://www.(letters or numbers or '-').(letters)/(letters or numbers)
-	var siteRegEx = new RegExp('(https|http):\/\/www.[a-zA-Z0-9-]+\.[^\s]{2,}(\/[a-zA-Z0-9-]+)?');
-	
-	//Allows comma seperated keywords made of numbers and letters
-	var keyRegEx = new RegExp('[0-9a-zA-Z]+(,[0-9a-zA-Z]+)*');
-	
-	//Allows any number for depth
-	var depthRegEx = new RegExp('^[0-9]+$');
-	
-	//Gets the values from the input fields
-	var nameInput = crawlNameInput.value;
-    var startInput = startUrlInput.value;
-    var keyInput = keywordInput.value;
-    var depthInput = crawlDepthInput.value;
-	
-	if(!nameRegEx.exec(nameInput)) {
-		inputsValid = false;
-		notification.innerHTML = 'Invalid name';
-	}
-	
-	if(!siteRegEx.exec(startInput)) {
-		inputsValid = false;
-		notification.innerHTML += '\n Invalid url';
-	}
-	
-	if(!keyRegEx.exec(keyInput)) {
-		inputsValid = false;
-		notification.innerHTML += '\n Invalid keys';
-	}
-	
-	if(!depthRegEx.exec(depthInput)) {
-		inputsValid = false;
-		notification.innerHTML += '\n Invalid depth';
-	}
-	
-	return inputsValid;
-}
+
